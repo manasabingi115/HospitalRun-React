b85d7f9aaabd581df2ba3dbdcb471e73d9948f14
Manasa Bingi <67860501+manasabingi115@users.noreply.github.com>
2022-06-22 18:28:10 +0530

    working in new patient component.

--- src/newPatient.js
+++ src/newPatient.js
@@ -1,9 +1,84 @@
 import React from "react";
 
 export default function NewPatient() {
+  function BasicInfo() {
+    return (
+      <div>
+        <p>Basic Information</p>
+        <div>
+          <div>
+            <p>Prefix</p>
+            <input
+              className="input is-primary"
+              type="text"
+              placeholder="Prefix"
+            />
+          </div>
+          <div>
+            <p>Given Name</p>
+            <input
+              className="input is-primary"
+              type="text"
+              placeholder="Given Name"
+            />
+          </div>
+          <div>
+            <p>Family Name</p>
+            <input
+              className="input is-primary"
+              type="text"
+              placeholder="Family Name"
+            />
+          </div>
+          <div>
+            <p>Suffix</p>
+            <input
+              className="input is-primary"
+              type="text"
+              placeholder="Suffix"
+            />
+          </div>
+        </div>
+        <div>
+          <div>
+            <p>Sex</p>
+            <input placeholder="Sex"></input>
+          </div>
+          <div>
+            <p>Patient Type</p>
+            <input placeholder="Patient Type"></input>
+          </div>
+          <div>
+            <p>Blood Type</p>
+            <input placeholder="Blood Type"></input>
+          </div>
+        </div>
+        <div>
+          <div>
+            <p>Date of Birth</p>
+            <input type="date"></input>
+          </div>
+          <div>
+            <p>Occupation</p>
+            <input placeholder="Occupation"></input>
+          </div>
+          <div>
+            <p>Preferred Language</p>
+            <input placeholder="Preferred Language"></input>
+            <div>
+              <input type="checkbox"></input>
+              <p>Unknown</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1>New Patient</h1>
+      <BasicInfo />
     </div>
   );
 }
--- src/patients.js
+++ src/patients.js
@@ -1,12 +1,20 @@
 import React from "react";
+import NewPatient from "./newPatient";
 
 export default function Patients() {
+  const [newPatient, setNewPatient] = React.useState(false);
+  console.log(newPatient);
+
   return (
     <div className="main-div">
       <h2>Patients</h2>
       <input placeholder="Seach"></input>
       <br />
-      <button>New Patient</button>
+      {newPatient === false && (
+        <button onClick={() => setNewPatient(true)}>New Patient</button>
+      )}
+      <button onClick={() => setNewPatient(false)}>Cancel</button>
+      {newPatient && <NewPatient />}
     </div>
   );
 }
