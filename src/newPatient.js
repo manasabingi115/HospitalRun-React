95ce07e604404e032bc396f90ab1605ab2b5dbb0
Manasa Bingi <67860501+manasabingi115@users.noreply.github.com>
2022-06-22 15:41:18 +0530

    updating

---
+++ src/newPatient.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NewPatient() {
+  return (
+    <div>
+      <h1>New Patient</h1>
+    </div>
+  );
+}
--- src/patients.js
+++ src/patients.js
@@ -4,6 +4,9 @@ export default function Patients() {
   return (
     <div className="main-div">
       <h2>Patients</h2>
+      <input placeholder="Seach"></input>
+      <br />
+      <button>New Patient</button>
     </div>
   );
 }
