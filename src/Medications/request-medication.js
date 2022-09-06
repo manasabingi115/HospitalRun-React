a474fc4da5171618288ad8df33c89f36a1a8ac26
Manasa Bingi <67860501+manasabingi115@users.noreply.github.com>
2022-09-06 18:10:23 +0530

    developing page

--- src/Medications/request-medication.js
+++ src/Medications/request-medication.js
@@ -6,8 +6,37 @@ export default function RequestMedication() {
       <h2>Request Medication</h2>
       <form>
         <p>Patient</p>
+        <input className="inputs-in-medications" placeholder="Patient"></input>
         <p>Medication</p>
+        <input
+          className="inputs-in-medications"
+          placeholder="Medication"
+        ></input>
         <p>Status</p>
+        <select className="inputs-in-medications">
+          <option>---Choose---</option>
+          <option>Draft</option>
+          <option>Active</option>
+        </select>
+        <p>Intent</p>
+        <select className="inputs-in-medications">
+          <option>Proposal</option>
+          <option>Plan</option>
+          <option>Order</option>
+          <option>Original Order</option>
+          <option>Reflex Order</option>
+          <option>Filler Order</option>
+          <option>Instance Order</option>
+          <option>Option</option>
+        </select>
+        <p>Priority</p>
+        <select className="inputs-in-medications">
+          <option>---Choose---</option>
+          <option>Draft</option>
+          <option>Active</option>
+        </select>
+        <p>Quantity | Value</p>
+        <input className="inputs-in-medications"></input>
       </form>
     </div>
   );
