1024d4f0d90b5f5b4970e128cd5964dd959e8cfc
Manasa Bingi <67860501+manasabingi115@users.noreply.github.com>
2022-09-06 18:20:33 +0530

    added code

--- src/Medications/request-medication.js
+++ src/Medications/request-medication.js
@@ -7,17 +7,23 @@ export default function RequestMedication() {
       <form>
         <p>Patient</p>
         <input className="inputs-in-medications" placeholder="Patient"></input>
+        <br />
+        <br />
         <p>Medication</p>
         <input
           className="inputs-in-medications"
           placeholder="Medication"
         ></input>
+        <br />
+        <br />
         <p>Status</p>
         <select className="inputs-in-medications">
           <option>---Choose---</option>
           <option>Draft</option>
           <option>Active</option>
         </select>
+        <br />
+        <br />
         <p>Intent</p>
         <select className="inputs-in-medications">
           <option>Proposal</option>
@@ -29,14 +35,20 @@ export default function RequestMedication() {
           <option>Instance Order</option>
           <option>Option</option>
         </select>
+        <br />
+        <br />
         <p>Priority</p>
         <select className="inputs-in-medications">
           <option>---Choose---</option>
           <option>Draft</option>
           <option>Active</option>
         </select>
+        <br />
+        <br />
         <p>Quantity | Value</p>
         <input className="inputs-in-medications"></input>
+        <br />
+        <br />
       </form>
     </div>
   );
