af1685c3f6744292a404a638754ba6697cf8b9bc
Manasa Bingi <67860501+manasabingi115@users.noreply.github.com>
2022-09-07 15:33:07 +0530

    code added

--- src/Medications/request-medication.js
+++ src/Medications/request-medication.js
@@ -7,48 +7,63 @@ export default function RequestMedication() {
       <form>
         <p>Patient</p>
         <input className="inputs-in-medications" placeholder="Patient"></input>
-        <br />
-        <br />
+
         <p>Medication</p>
         <input
           className="inputs-in-medications"
           placeholder="Medication"
         ></input>
-        <br />
-        <br />
+
         <p>Status</p>
         <select className="inputs-in-medications">
-          <option>---Choose---</option>
-          <option>Draft</option>
-          <option>Active</option>
+          <option className="options-in-medication">---Choose---</option>
+          <option className="options-in-medication">Draft</option>
+          <option className="options-in-medication">Active</option>
         </select>
-        <br />
-        <br />
+
         <p>Intent</p>
         <select className="inputs-in-medications">
-          <option>Proposal</option>
-          <option>Plan</option>
-          <option>Order</option>
-          <option>Original Order</option>
-          <option>Reflex Order</option>
-          <option>Filler Order</option>
-          <option>Instance Order</option>
-          <option>Option</option>
+          <option className="options-in-medication">Proposal</option>
+          <option className="options-in-medication">Plan</option>
+          <option className="options-in-medication" selected>
+            Order
+          </option>
+          <option className="options-in-medication">Original Order</option>
+          <option className="options-in-medication">Reflex Order</option>
+          <option className="options-in-medication">Filler Order</option>
+          <option className="options-in-medication">Instance Order</option>
+          <option className="options-in-medication">Option</option>
         </select>
-        <br />
-        <br />
+
         <p>Priority</p>
         <select className="inputs-in-medications">
-          <option>---Choose---</option>
-          <option>Draft</option>
-          <option>Active</option>
+          <option className="options-in-medication">---Choose---</option>
+          <option className="options-in-medication">Routine</option>
+          <option className="options-in-medication">Urgent</option>
+          <option className="options-in-medication">Asap</option>
+          <option className="options-in-medication">Stat</option>
         </select>
-        <br />
-        <br />
-        <p>Quantity | Value</p>
-        <input className="inputs-in-medications"></input>
-        <br />
-        <br />
+        <div className="parent-to-child-inputs-in-medications">
+          <div
+            className="child-div-in-medications"
+            style={{ paddingRight: "30px" }}
+          >
+            <p>Quantity | Value</p>
+            <input
+              className="child-input-in-medications"
+              placeholder="Quantity | Value"
+            ></input>
+          </div>
+          <div className="child-div-in-medications">
+            <p>Quantity | Unit</p>
+            <input
+              className="child-input-in-medications"
+              placeholder="Quantity | Unit"
+            ></input>
+          </div>
+        </div>
+        <p>Notes</p>
+        <textarea className="textarea-in-medications"></textarea>
       </form>
     </div>
   );
